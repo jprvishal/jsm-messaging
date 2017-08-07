0e6ea115ef5f68717a9f6dd74c2f3522ce0b65a3
jprvishal <jpr.vishal@gmail.com>
2017-08-07 17:54:59 -0500

    New project

---
+++ jms-messaging/src/main/java/hello/Application.java
@@ -0,0 +1,46 @@
+package hello;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configure) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configure.configure(factory, connectionFactory);
+		
+		return factory;
+	}
+	
+	@Bean
+	public MessageConverter jaksonJmsMesssageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		
+		return converter;
+	}
+	
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		JmsTemplate template = context.getBean(JmsTemplate.class);
+		System.out.println("Sending an email message");
+		template.convertAndSend("mailbox", new Email("test.gmail.com", "This is a test JMS message"));
+	}
+}
